65bc9edcefc4f190fa79fdf7db8d350c47ec8f3d
jai <jai@Jai-LM>
2018-03-24 22:37:43 +1030

    Load shaders from file

---
+++ src/main/java/mod/id107/raytracer/Reader.java
@@ -0,0 +1,32 @@
+package mod.id107.raytracer;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Reader {
+
+	public static String read(String resourceIn) {
+		InputStream is = Reader.class.getResourceAsStream(resourceIn);
+		if (is == null) {
+			Log.info("Shader not found");
+			return "";
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		int i;
+		
+		try {
+			i = is.read();
+			while (i != -1) {
+				sb.append((char) i);
+				i = is.read();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+			
+		}
+		
+		return sb.toString();
+	}
+}

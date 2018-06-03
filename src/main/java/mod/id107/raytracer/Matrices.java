b1f0285c1969bbd3a580043bbe85f16d1cda6e48
jai <jai@Jai-LM>
2018-06-03 12:37:24 +0930

    Fixed rotation

--- src/main/java/mod/id107/raytracer/Matrices.java
+++ src/main/java/mod/id107/raytracer/Matrices.java
@@ -19,20 +19,18 @@ public class Matrices {
 	
 	private Matrix4f xMirror;
 	
-	//FIXME only 48 matrices needed
-	
 	public Matrices() {
-		xMirror = new Matrix4f(-1,0,0,0, 0,1,0,0, 0,0,1,0, 1,0,0,1); //flip
+		xMirror = new Matrix4f(-1,0,0,1, 0,1,0,0, 0,0,1,0, 0,0,0,1); //mirror
 		
-		z01 = new Matrix4f(0,1,0,0, -1,0,0,0, 0,0,1,0, 1,0,0,1); //right
-		z10 = new Matrix4f(0,-1,0,0, 1,0,0,0, 0,0,1,0, 0,1,0,1); //left
-		z11 = new Matrix4f(-1,0,0,0, 0,-1,0,0, 0,0,1,0, 1,1,0,1); //over
+		z01 = new Matrix4f(0,-1,0,1, 1,0,0,0, 0,0,1,0, 0,0,0,1); //right
+		z10 = new Matrix4f(0,1,0,0, -1,0,0,1, 0,0,1,0, 0,0,0,1); //left
+		z11 = new Matrix4f(-1,0,0,1, 0,-1,0,1, 0,0,1,0, 0,0,0,1); //over
 		
-		y01 = new Matrix4f(0,0,1,0, 0,1,0,0, -1,0,0,0, 1,0,0,1);  //east
-		y10 = new Matrix4f(0,0,-1,0, 0,1,0,0, 1,0,0,0, 0,0,1,1);  //west
-		y11 = new Matrix4f(-1,0,0,0, 0,1,0,0, 0,0,-1,0, 1,0,1,1); //north
-		x01 = new Matrix4f(1,0,0,0, 0,0,1,0, 0,-1,0,0, 0,1,0,1); //up
-		x10 = new Matrix4f(1,0,0,0, 0,0,-1,0, 0,1,0,0, 0,0,1,1); //down
+		y01 = new Matrix4f(0,0,-1,1, 0,1,0,0, 1,0,0,0, 0,0,0,1);  //east
+		y10 = new Matrix4f(0,0,1,0, 0,1,0,0, -1,0,0,1, 0,0,0,1);  //west
+		y11 = new Matrix4f(-1,0,0,1, 0,1,0,0, 0,0,-1,1, 0,0,0,1); //north
+		x01 = new Matrix4f(1,0,0,0, 0,0,-1,1, 0,1,0,0, 0,0,0,1); //up
+		x10 = new Matrix4f(1,0,0,0, 0,0,1,0, 0,-1,0,1, 0,0,0,1); //down
 	}
 	
 	public String getMatrices() {
@@ -40,6 +38,22 @@ public class Matrices {
 		sb.append("\nconst mat4 rotation[48] = mat4[](\n");
 		for (int i = 0; i < 48; i++) {
 			Matrix4f matrix = new Matrix4f(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1);
+			
+			if ((i&1)==1) {
+				Matrix4f.mul(xMirror, matrix, matrix);
+			}
+			
+			switch((i>>1)&3) {
+			case 1:
+				Matrix4f.mul(z01, matrix, matrix);
+				break;
+			case 2:
+				Matrix4f.mul(z10, matrix, matrix);
+				break;
+			case 3:
+				Matrix4f.mul(z11, matrix, matrix);
+				break;
+			}
 			
 			switch(i>>3) {
 			case 1:
@@ -57,22 +71,6 @@ public class Matrices {
 			case 5:
 				Matrix4f.mul(x10, matrix, matrix);
 				break;
-			}
-			
-			switch((i>>1)&3) {
-			case 1:
-				Matrix4f.mul(z01, matrix, matrix);
-				break;
-			case 2:
-				Matrix4f.mul(z10, matrix, matrix);
-				break;
-			case 3:
-				Matrix4f.mul(z11, matrix, matrix);
-				break;
-			}
-			
-			if ((i&1)==1) {
-				Matrix4f.mul(xMirror, matrix, matrix);
 			}
 			
 			float[] f = matrix.get();
